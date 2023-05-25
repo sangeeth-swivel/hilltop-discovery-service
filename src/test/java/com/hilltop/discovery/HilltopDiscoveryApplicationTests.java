989c3627080ec76226bafbf5a8ab60757d157566
SangeethArulraj <sangeeth@swivelgroup.com.au>
2023-05-25 14:20:49 +0530

    Initial commit

---
+++ src/test/java/com/hilltop/discovery/HilltopDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilltop.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HilltopDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
